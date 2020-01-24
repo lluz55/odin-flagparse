81a44c127838fd8caeb7c2f6303acab644c65a30
kim (grufwub) <grufwub@gmail.com>
2020-01-24 13:49:09 +0000

    add script to automate build testing
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+print_exit() {
+    printf '%s\n' "$1"
+    exit $2
+}
+
+odin build test.odin || print_exit 'Failed to compile!\n' 1
+printf 'Compiled test.odin!\n'
+
+printf '\nTesting values: string="a string" bool=true int=999 uint=999\n'
+./test --string 'a string' --bool true --int 999 --uint 999
+
+printf '\nTesting values: int=nope uint=111\n'
+./test --int nope --uint 111
+
+printf '\nTesting values: int=111 uint=nope\n'
+./test --int 111 --uint nope
+
+unset print_exit
