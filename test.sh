eddc0af8ce10b34dc2a0ea5fca3d5e426c26acfb
kim (grufwub) <grufwub@gmail.com>
2020-01-25 17:15:28 +0000

    update test.sh to match latest argparse changes
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ odin build test.odin || print_exit 'Failed to compile!\n' 1
 printf 'Compiled test.odin!\n'
 
 printf '\nTesting values: string="a string" bool=true int=999 uint=999\n'
-./test --string 'a string' --bool true --int 999 --uint 999
+./test --string 'a string' --bool --int 999 --uint 999
 
 printf '\nTesting values: int=nope uint=111\n'
 ./test --int nope --uint 111
